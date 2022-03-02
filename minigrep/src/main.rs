eb2fddccf058c93c9ff66a8a6a7d3cee7165bf54
Afshan ahmed khan <afshanahmeda2k@gmail.com>
2022-03-02 19:37:57 +0530

    initial commit

---
+++ minigrep/src/main.rs
@@ -0,0 +1,19 @@
+use std::env;
+use std::process;
+
+use minigrep_redoca2k::Config;
+
+fn main() {
+    //let args: Vec<String> = env::args().collect();
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments : {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep_redoca2k::run(config){
+        eprintln!("Application error : {}",e);
+        process::exit(1);
+    }
+}
+
+
